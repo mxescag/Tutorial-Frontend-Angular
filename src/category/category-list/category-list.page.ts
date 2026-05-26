021e86ab76e49f2a6ca5ca514bc72130cadf46ca
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-26 14:36:29 +0200

    build(category-list): add dependencies and implementations

--- src/category/category-list/category-list.page.ts
+++ src/category/category-list/category-list.page.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Category } from '../model/Category';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-category-list',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTableModule, CommonModule],
   templateUrl: './category-list.page.html',
   styleUrl: './category-list.page.scss',
 })
-export class CategoryListPage {}
+export class CategoryListPage implements OnInit {
+  dataSource = new MatTableDataSource<Category>(); /* Aquí se almacenarán los datos que se mostrarán en la tabla. */
+  displayedColumns: string[] = ['id', 'name', 'action']; /* Aquí se definen las columnas que se mostrarán en la tabla. */
+
+  constructor() {}
+
+  ngOnInit(): void {} /* Aquí se pueden cargar los datos desde un servicio o API. */
+}
