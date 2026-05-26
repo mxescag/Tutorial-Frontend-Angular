ed94ab48c71f3eb1944ea65ba13edbafcf16d59b
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-26 23:56:32 +0200

    build(category): implement connection with Backend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http'
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection ({eventCoalescing : true}), /* Esto es para mejorar el rendimiento de la aplicación al reducir la cantidad de veces que Angular tiene que realizar la detección de cambios. */
+    provideBrowserGlobalErrorListeners(), /* Esto es para manejar los errores globales en la aplicación. */
+    provideHttpClient(withFetch()), /* Esto es para configurar el cliente HTTP de Angular para que utilice la API Fetch en lugar de XMLHttpRequest. */
+    provideRouter(routes) /* Esto es para configurar el enrutador de Angular con las rutas definidas en el archivo app.routes.ts. */
   ]
 };
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,24 +1,34 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Category } from './model/Category';
 import { CATEGORY_DATA } from './model/mock-categories';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
+  /*  -- INYECCIONES --*/
+
+  protected readonly http = inject(HttpClient);
+  private baseUrl = 'http://localhost:8080/category'
+
   constructor() {}
 
   getCategories(): Observable<Category[]> {
-    // Aquí se implementaría la lógica para obtener las categorías desde un servicio o API.
-    return of(CATEGORY_DATA); 
+    return this.http.get<Category[]>(this.baseUrl);
+    /* Explicación paso por paso de lo que hace esto:
+    this.http.get<Category[]>(this.baseUrl) hace una solicitud HTTP GET a la URL especificada en this.baseUrl. 
+    El método get devuelve un Observable que emitirá un array de objetos Category cuando se reciba la respuesta del servidor. 
+    El tipo <Category[]> indica que se espera (!!!) que la respuesta sea un array de objetos Category.
+    */
   }
 
   saveCategory(category: Category): Observable<Category> {
     return of(null);
   }
 
-  deleteCategory(idCategory : number): Observable<any> {
+  deleteCategory(idCategory: number): Observable<any> {
     return of(null);
-  } 
+  }
 }
