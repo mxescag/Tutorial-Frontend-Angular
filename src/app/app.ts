279b60fb7bb8f7cd02a215ceb10afd54f974b704
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-26 14:10:30 +0200

    build: add Material dependency

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('tutorial');
+  protected readonly title = signal('Tutorial de Angular');
 }
