35c6b56936dece0e77298b25fe9ee5791fc8332e
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-26 14:21:31 +0200

    feat(category): add category list page

---
+++ src/category/category-list/category-list.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-list',
+  imports: [],
+  templateUrl: './category-list.page.html',
+  styleUrl: './category-list.page.scss',
+})
+export class CategoryListPage {}
