6e6261254831dcc067c78ab16ce32e9ab50db507
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-26 23:40:42 +0200

    build(core): implement dialog-confirmation and methods

--- src/core/dialog-confirmation/dialog-confirmation.ts
+++ src/core/dialog-confirmation/dialog-confirmation.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-dialog-confirmation',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './dialog-confirmation.html',
   styleUrl: './dialog-confirmation.scss',
 })
-export class DialogConfirmation {}
+export class DialogConfirmation {
+  protected readonly title = signal<string | null>(null); /* Título puede ser null (así inicializado) o string */
+  protected readonly description = signal<string | null>(null); /* Mismo con descripción */
+
+  protected readonly dialogRef = inject(MatDialogRef<DialogConfirmation>);
+  protected readonly data = inject(MAT_DIALOG_DATA);
+
+  ngOnInit(): void {
+    this.title.set(this.data.title); /* Asignamos el título recibido en data al signal title */
+    this.description.set(this.data.description); /* Asignamos la descripción recibida en data al signal description */
+  }
+
+  /* Método para cerrar el diálogo, opcionalmente pasando un valor (por defecto false) */
+  onClose(value = false) {
+    this.dialogRef.close(value);
+  }
+}
