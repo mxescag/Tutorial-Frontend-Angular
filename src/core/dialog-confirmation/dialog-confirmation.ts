d258a4cc65373f49a90395fcc4efe6593e03752d
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-26 23:31:12 +0200

    build(core): add dialog-confirmation

---
+++ src/core/dialog-confirmation/dialog-confirmation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-confirmation',
+  imports: [],
+  templateUrl: './dialog-confirmation.html',
+  styleUrl: './dialog-confirmation.scss',
+})
+export class DialogConfirmation {}
