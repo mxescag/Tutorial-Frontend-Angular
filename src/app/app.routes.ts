2a4175eaff8e07659ed54730702987f6c7b16d29
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-26 14:27:33 +0200

    feat(category-list): add route to app-routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+/* Aquí añadimos las rutas a los componentes que vayamos añadiendo. */
+export const routes: Routes = [
+    /* Explicación de la falla:
+     - path: cómo se llamará en URL
+     - loadComponent: () : función anónima que importará la página de category-list 
+     - .then() : entonces, una vez esté importada, se utilizará el componente que está en esa clase */
+    { path: 'categories', loadComponent: () => import('../category/category-list/category-list.page').then(m => m.CategoryListPage)}
+];
