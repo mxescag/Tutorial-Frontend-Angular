a0feab958e57f24d6640c21d498ce4ca423a2838
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-26 14:55:05 +0200

    build(category-list): implemented createCategory method

--- src/category/category-list/category-list.page.ts
+++ src/category/category-list/category-list.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Category } from '../model/Category';
 import { CommonModule } from '@angular/common';
@@ -6,6 +6,8 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CategoryService } from '../category.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CategoryEdit } from '../category-edit/category-edit';
 
 @Component({
   selector: 'app-category-list',
@@ -14,12 +16,27 @@ import { CategoryService } from '../category.service';
   styleUrl: './category-list.page.scss',
 })
 export class CategoryListPage implements OnInit {
+
+  protected readonly categoryService = inject(CategoryService); /* Aquí se inyecta el servicio de categorías para poder obtener los datos de las categorías. */
+  protected readonly dialog = inject(MatDialog); /* Aquí se inyecta el servicio de diálogo para poder abrir el diálogo de edición de categorías. */
+
   dataSource = new MatTableDataSource<Category>(); /* Aquí se almacenarán los datos que se mostrarán en la tabla. */
   displayedColumns: string[] = ['id', 'name', 'action']; /* Aquí se definen las columnas que se mostrarán en la tabla. */
 
-  constructor(
-    private categoryService: CategoryService /* Aquí se inyecta el servicio para obtener los datos. */
-  ) {}
+  constructor() {}
+
+  /* -- DEFINIMOS MÉTODOS POR AQUÍ -- */
+
+  createCategory() {
+    const dialogRef = this.dialog.open(CategoryEdit, {
+      data:{}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(!result) return; /* Si el resultado es falso, no se hace nada. Esto puede ocurrir si el usuario cierra el diálogo sin guardar. */
+      this.loadData(); /* Si el resultado es verdadero, se recarga la lista de categorías para mostrar la nueva categoría creada. */
+    });
+  }
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe((categories) => {
