2fb9dea94acd30facc3d457ec6f3c0617fa75b0e
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-27 13:33:28 +0200

    feat(game): implement game-edit

--- src/game/game-edit/game-edit.ts
+++ src/game/game-edit/game-edit.ts
@@ -1,9 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { GameService } from '../game.service';
+import { Game } from '../model/Game';
+import { AuthorService } from '../../author/author.service';
+import { Author } from '../../author/model/Author';
+import { CategoryService } from '../../category/category.service';
+import { Category } from '../../category/model/Category';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { validateFields } from '../../core/helpers/validation.helper';
 
 @Component({
-  selector: 'app-game-edit',
-  imports: [],
-  templateUrl: './game-edit.html',
-  styleUrl: './game-edit.scss',
+    selector: 'app-game-edit',
+    standalone: true,
+    imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule ],
+    templateUrl: './game-edit.html',
+    styleUrl: './game-edit.scss',
 })
-export class GameEdit {}
+export class GameEdit implements OnInit {
+    protected readonly id = signal<number | null>(null);
+    protected readonly title = signal<string | null>(null);
+    protected readonly age = signal<number | null>(null);
+    protected readonly categoryId = signal<number | null>(null);
+    protected readonly authorId = signal<number | null>(null);
+    protected readonly categories = signal<Category[]>([]);
+    protected readonly authors = signal<Author[]>([]);
+
+    protected readonly dialogRef = inject(MatDialogRef<GameEdit>);
+    protected readonly data = inject(MAT_DIALOG_DATA);
+    protected readonly gameService = inject(GameService);
+    protected readonly categoryService = inject(CategoryService);
+    protected readonly authorService = inject(AuthorService);
+
+    ngOnInit(): void {
+        this.loadFormData(this.data.game ?? null);
+    }
+
+    loadFormData(initialData: Game | null): void {
+        this.id.set(initialData?.id ?? null);
+        this.title.set(initialData?.title ?? null);
+        this.age.set(initialData?.age ?? null);
+
+        this.categoryService.getCategories().subscribe((cats) => {
+            this.categories.set(cats);
+            this.categoryId.set(initialData?.category?.id ?? null);
+        });
+
+        this.authorService.getAllAuthors().subscribe((auts) => {
+            this.authors.set(auts);
+            this.authorId.set(initialData?.author?.id ?? null);
+        });
+    }
+
+    onSave() {
+        const id = this.id();
+        const title = this.title(); 
+        const age = this.age(); 
+        const categoryId = this.categoryId(); 
+        const authorId = this.authorId(); 
+
+        const requiredFields = ["title", "age", "categoryId", "authorId"] as const
+        const data = { title, age, categoryId, authorId }
+
+        if (!validateFields(data, requiredFields)) {
+            return;
+        }
+
+        const game = {
+            id,
+            title,
+            age,
+            category: this.categories().find(c => c.id === categoryId) ?? null,
+            author: this.authors().find(a => a.id === authorId) ?? null,
+        } as Game;
+        this.gameService.saveGame(game).subscribe(() => {
+            this.dialogRef.close(true);
+        });
+    }
+
+    onClose() {
+        this.dialogRef.close();
+    }
+}
