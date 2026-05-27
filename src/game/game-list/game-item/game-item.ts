e85aacc94ed038a1ab20d13424c1f9fc0a1f926a
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-27 13:29:23 +0200

    feat(game): implement game-item

--- src/game/game-list/game-item/game-item.ts
+++ src/game/game-list/game-item/game-item.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Game } from '../../model/Game';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'app-game-item',
-  imports: [],
-  templateUrl: './game-item.html',
-  styleUrl: './game-item.scss',
+    selector: 'app-game-item',
+    standalone: true,
+    imports: [MatCardModule],
+    templateUrl: './game-item.html',
+    styleUrl: './game-item.scss'
 })
-export class GameItem {}
+export class GameItem {
+    protected readonly game = input.required<Game>();
+}
