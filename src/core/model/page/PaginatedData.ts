4df4be4166bc2c6c3e7b59f8aa853e852ff581f7
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-27 11:24:55 +0200

    add comments

--- src/core/model/page/PaginatedData.ts
+++ src/core/model/page/PaginatedData.ts
@@ -1,6 +1,7 @@
 import { Pageable } from './Pageable';
 
-export interface PaginatedData <TData> {
+/* TData es un genérico de TS. Permite que esta clase sea reutilizable con cualquier tipo de contenido. */
+export interface PaginatedData <TData> { 
     content: TData[];
     pageable: Pageable;
     totalElements: number;
