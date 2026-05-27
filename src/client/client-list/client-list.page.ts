28f09f45c092c1b5b8df7a43c67879fef2ad46be
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-27 14:25:22 +0200

    feat(client): implement list

--- src/client/client-list/client-list.page.ts
+++ src/client/client-list/client-list.page.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Client } from '../model/Client';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-client-list',
-  imports: [],
+  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule],
   templateUrl: './client-list.page.html',
   styleUrl: './client-list.page.scss',
 })
-export class ClientListPage {}
+export class ClientListPage implements OnInit {
+
+  dataSource = new MatTableDataSource<Client>();
+  displayedColumns: string[] = ['id', 'name', 'action'];
+
+  ngOnInit(): void {
+    }
+}
