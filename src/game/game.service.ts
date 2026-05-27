4252288bead9c3ab2bf7617bbc155bb5c5dd473f
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-27 13:17:24 +0200

    feat(game): add mock games and save and get methods in service

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Game } from './model/Game';
+import { GAME_DATA } from './model/mock-games';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root'
 })
-export class Game {}
+export class GameService {
+
+    constructor() { }
+
+    getGames(title?: string, categoryId?: number): Observable<Game[]> {
+        return of(GAME_DATA);
+    }
+
+    saveGame(game: Game): Observable<void> {
+        return of(null);
+    }
+
+}
