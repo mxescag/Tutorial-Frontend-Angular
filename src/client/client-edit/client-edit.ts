d11e7a33f38b5b9f979a8f993940e1aba1a4be0c
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-27 14:13:46 +0200

    feat(client): add client list, edit, and service

---
+++ src/client/client-edit/client-edit.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-edit',
+  imports: [],
+  templateUrl: './client-edit.html',
+  styleUrl: './client-edit.scss',
+})
+export class ClientEdit {}
---
+++ src/client/client-list/client-list.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-list',
+  imports: [],
+  templateUrl: './client-list.page.html',
+  styleUrl: './client-list.page.scss',
+})
+export class ClientListPage {}
