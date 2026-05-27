6c50b7517984db9d0fef364966c485cb56402b96
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-27 11:33:22 +0200

    add get, save and delete methods

--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pageable } from '../core/model/page/Pageable';
+import { Author } from './model/Author';
+import { PaginatedData } from '../core/model/page/PaginatedData';
+import { AUTHOR_DATA } from './model/mock-authors';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthorService {}
+export class AuthorService {
+  constructor(){}
+
+  getAuthors(pageable: Pageable): Observable<PaginatedData<Author>> {
+    return of(AUTHOR_DATA);
+  }
+
+  saveAuthor(author: Author): Observable<void> {
+    return of(null);
+  }
+
+  deleteAuthor(idAuthor: number): Observable<void> {
+    return of(null);
+  }
+}
