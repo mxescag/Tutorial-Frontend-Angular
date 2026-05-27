a05a94d013ee99c4bdde3cf05b5464c7291c8630
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-27 11:57:50 +0200

    feat(author): implement author-list and author-edit

--- src/author/author-edit/author-edit.ts
+++ src/author/author-edit/author-edit.ts
@@ -1,9 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AuthorService } from '../author.service';
+import { Author } from '../model/Author';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { validateFields } from '../../core/helpers/validation.helper';
 
 @Component({
-  selector: 'app-author-edit',
-  imports: [],
-  templateUrl: './author-edit.html',
-  styleUrl: './author-edit.scss',
+    selector: 'app-author-edit',
+    standalone: true,
+    imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+    templateUrl: './author-edit.html',
+    styleUrl: './author-edit.scss',
 })
-export class AuthorEdit {}
+export class AuthorEdit implements OnInit {
+    protected readonly authorService = inject(AuthorService);
+    protected readonly dialogRef = inject(MatDialogRef<AuthorEdit>);
+    protected readonly data = inject(MAT_DIALOG_DATA);
+
+    protected readonly id = signal<number | null>(null);
+    protected readonly name = signal<string | null>(null);
+    protected readonly nationality = signal<string | null>(null);
+author: any;
+
+    loadFormData(initialData: Author | null) {
+        this.id.set(initialData.id ?? null);
+        this.name.set(initialData.name ?? null);
+        this.nationality.set(initialData.nationality ?? null);
+    }
+
+    ngOnInit(): void {
+        this.loadFormData(this.data.author ?? null);
+    }
+
+    onSave() {
+        const id = this.id();
+        const name = this.name();
+        const nationality = this.nationality();
+
+        const requiredFields = ["name", "nationality"] as const
+        const data = { name, nationality }
+
+        if (!validateFields(data, requiredFields)) {
+            return;
+        }
+
+        const author = {
+            id,
+            name,
+            nationality,
+        } as Author;
+        this.authorService.saveAuthor(author).subscribe(() => {
+            this.dialogRef.close(true);
+        });
+    }
+
+    onClose() {
+        this.dialogRef.close(false);
+    }
+}
