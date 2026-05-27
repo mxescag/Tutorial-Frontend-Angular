b45acb508ce27b89f6bdd56d9334a49d258ea0c7
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-27 13:14:49 +0200

    feat(app.routes): add routes to games

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,15 @@ export const routes: Routes = [
      - path: cómo se llamará en URL
      - loadComponent: () : función anónima que importará la página de category-list 
      - .then() : entonces, una vez esté importada, se utilizará el componente que está en esa clase */
+     { path: '', redirectTo: '/games', pathMatch: 'full'}, /* Añadimos esta ruta para que vaya a games por defecto la página inicial. */
     { path: 'categories', 
         loadComponent: () => import('../category/category-list/category-list.page').then(m => m.CategoryListPage)},
     {
         path: 'authors',
         loadComponent: () => import ('../author/author-list/author-list.page').then(m => m.AuthorListPage)
+    },
+    {
+        path: 'games',
+        loadComponent: () => import ('../game/game-list/game-list.page').then(m => m.GameListPage)
     }
 ];
