ff0b4bedcad17bc14240bb606f35c127b4c39f89
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-28 12:41:07 +0200

    feat(loan): implement get, save and delete methods to service

--- src/loan/loan.service.ts
+++ src/loan/loan.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pageable } from '../core/model/page/Pageable';
+import { PaginatedData } from '../core/model/page/PaginatedData'
+import { Loan } from '../loan/model/Loan'
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class LoanService {}
+export class LoanService {
+
+  constructor(){}
+
+  getLoans(pageable: Pageable): Observable<PaginatedData<Loan>> {
+    return;
+  }
+
+  saveLoan(loan: Loan): Observable<void> {
+    return;
+  }
+
+  deleteLoan(idLoan: number): Observable<void> {
+    return;
+  }
+}
