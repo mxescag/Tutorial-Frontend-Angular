bd6b8a30c68ab15d2198d64657faab9b614706b5
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-29 11:40:25 +0200

    feat(loan): getLoans() can now filter by title, clientId and date

--- src/loan/loan.service.ts
+++ src/loan/loan.service.ts
@@ -13,8 +13,13 @@ export class LoanService {
   private baseUrl = 'http://localhost:8080/loan';
 
 
-  getLoans(pageable: Pageable): Observable<PaginatedData<Loan>> {
-    return this.http.post<PaginatedData<Loan>>(this.baseUrl, {pageable: pageable})
+  /* En esta función, los ? significan que los parámetros que lo lleven son opcionales. */
+  getLoans(pageable: Pageable, title?: string, clientId?: number, date?: string): Observable<PaginatedData<Loan>> {
+    return this.http.post<PaginatedData<Loan>>(this.baseUrl, {pageable,
+      title: title ?? null, // Si title es undefined, envía null
+      clientId: clientId ?? null, // Ídem
+      date: date ?? null // Ídem
+    });
   }
 
 
