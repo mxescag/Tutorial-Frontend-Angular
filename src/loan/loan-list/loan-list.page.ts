475f6e72358e6c2628c7e9b7b747a6310e76d146
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-29 11:07:54 +0200

    feat(loan): implemented filter for games and clients

--- src/loan/loan-list/loan-list.page.ts
+++ src/loan/loan-list/loan-list.page.ts
@@ -1,97 +1,129 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { LoanEdit } from '../loan-edit/loan-edit';
 import { LoanService } from '../loan.service';
 import { Loan } from '../model/Loan';
+import { Game } from '../../game/model/Game';
+import { GameService } from '../../game/game.service'
+import { Client } from '../../client/model/Client'
+import { ClientService } from '../../client/client.service'
 import { Pageable } from '../../core/model/page/Pageable';
 import { DialogConfirmation } from '../../core/dialog-confirmation/dialog-confirmation';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'app-loan-list',
-    standalone: true,
-    imports: [MatButtonModule, MatIconModule, MatTableModule, CommonModule, MatPaginator],
-    templateUrl: './loan-list.page.html',
-    styleUrl: './loan-list.page.scss',
+  selector: 'app-loan-list',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    FormsModule,
+    CommonModule,
+    MatPaginator,
+    MatFormField,
+    MatLabel,
+    MatSelectModule,
+  ],
+  templateUrl: './loan-list.page.html',
+  styleUrl: './loan-list.page.scss',
 })
 export class LoanListPage implements OnInit {
-    pageNumber: number = 0;
-    pageSize: number = 5;
-    totalElements: number = 0;
+  pageNumber: number = 0;
+  pageSize: number = 5;
+  totalElements: number = 0;
 
-    dataSource = new MatTableDataSource<Loan>();
-    displayedColumns: string[] = ['id', 'game', 'client', 'startDate', 'endDate', 'action'];
+  dataSource = new MatTableDataSource<Loan>();
+  displayedColumns: string[] = ['id', 'game', 'client', 'startDate', 'endDate', 'action'];
 
-    constructor(private loanService: LoanService, public dialog: MatDialog) {}
+  constructor(
+    private loanService: LoanService,
+    public dialog: MatDialog,
+  ) {}
 
-    ngOnInit(): void {
-        this.loadPage();
+  protected readonly gameService = inject(GameService);
+  protected readonly clientService = inject(ClientService);
+  protected readonly clients = signal<Client[]>([]);
+  protected readonly games = signal<Game[]>([]);
+
+  /* Filtros para títulos, juegos y fechas */
+  protected readonly filterClient = signal<Client | null>(null);
+  protected readonly filterTitle = signal<string>('');
+
+  ngOnInit(): void {
+    this.loadPage();
+    this.gameService.getGames().subscribe((games) => this.games.set(games));
+
+    this.clientService.getClients().subscribe((clients) => this.clients.set(clients));
+  }
+
+  loadPage(event?: PageEvent) {
+    const pageable: Pageable = {
+      pageNumber: this.pageNumber,
+      pageSize: this.pageSize,
+      sort: [
+        {
+          property: 'id',
+          direction: 'ASC',
+        },
+      ],
+    };
+
+    if (event != null) {
+      pageable.pageSize = event.pageSize;
+      pageable.pageNumber = event.pageIndex;
     }
 
-    loadPage(event?: PageEvent) {
-        const pageable: Pageable = {
-            pageNumber: this.pageNumber,
-            pageSize: this.pageSize,
-            sort: [
-                {
-                    property: 'id',
-                    direction: 'ASC',
-                },
-            ],
-        };
+    this.loanService.getLoans(pageable).subscribe((data) => {
+      this.dataSource.data = data.content;
+      this.pageNumber = data.pageable.pageNumber;
+      this.pageSize = data.pageable.pageSize;
+      this.totalElements = data.totalElements;
+    });
+  }
 
-        if (event != null) {
-            pageable.pageSize = event.pageSize;
-            pageable.pageNumber = event.pageIndex;
-        }
+  createLoan() {
+    const dialogRef = this.dialog.open(LoanEdit, {
+      data: {},
+    });
 
-        this.loanService.getLoans(pageable).subscribe((data) => {
-            this.dataSource.data = data.content;
-            this.pageNumber = data.pageable.pageNumber;
-            this.pageSize = data.pageable.pageSize;
-            this.totalElements = data.totalElements;
+    dialogRef.afterClosed().subscribe((result) => {
+      this.ngOnInit();
+    });
+  }
+
+  editLoan(loan: Loan) {
+    const dialogRef = this.dialog.open(LoanEdit, {
+      data: { loan: loan },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.ngOnInit();
+    });
+  }
+
+  deleteLoan(loan: Loan) {
+    const dialogRef = this.dialog.open(DialogConfirmation, {
+      data: {
+        title: 'Eliminar préstamo',
+        description:
+          '¡Atención! Si borra el préstamo, se perderán sus datos.<br> ¿Desea eliminar el préstamo?',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.loanService.deleteLoan(loan.id).subscribe((result) => {
+          this.ngOnInit();
         });
-    }
-
-    createLoan() {
-        const dialogRef = this.dialog.open(LoanEdit, {
-            data: {},
-        });
-
-        dialogRef.afterClosed().subscribe((result) => {
-            this.ngOnInit();
-        });
-    }
-
-    editLoan(loan: Loan) {
-        const dialogRef = this.dialog.open(LoanEdit, {
-            data: { loan: loan },
-        });
-
-        dialogRef.afterClosed().subscribe((result) => {
-            this.ngOnInit();
-        });
-    }
-
-    deleteLoan(loan: Loan) {
-        const dialogRef = this.dialog.open(DialogConfirmation, {
-            data: {
-                title: 'Eliminar préstamo',
-                description:
-                    '¡Atención! Si borra el préstamo, se perderán sus datos.<br> ¿Desea eliminar el préstamo?',
-            },
-        });
-
-        dialogRef.afterClosed().subscribe((result) => {
-            if (result) {
-                this.loanService.deleteLoan(loan.id).subscribe((result) => {
-                    this.ngOnInit();
-                });
-            }
-        });
-    }
+      }
+    });
+  }
 }
